99c6beac0d2121fc9255cc576aadb3a841dbd846
Gilibeth Vega <gvega@creditu.com>
2020-11-19 21:27:53 -0300

    se simplifican consultas

--- prueba.sql
+++ prueba.sql
@@ -531,73 +531,40 @@ INSERT INTO factura_producto(
 
 --- Comprobando relación entre tablas
 
--- ¿Que cliente realizó la compra más cara? PD: Se da el subtotal de la factura (no se calcula impuesto)
-SELECT y_cliente.nombre, cliente_id_compra_mas_alta.factura_numero, cliente_id_compra_mas_alta.subtotal_factura_mayor
+-- ¿Que cliente realizó la compra más cara? 
+SELECT total_factura_mayor, factura_numero, cliente_id, nombre
 FROM (
-  SELECT y_factura.cliente_id, factura_con_compra_mas_cara.factura_numero, factura_con_compra_mas_cara.subtotal_factura_mayor
+  SELECT total_factura_mayor, factura_numero, cliente_id 
   FROM (
-    SELECT
-      sum(total_prod) AS subtotal_factura_mayor, factura_numero 
-    FROM (
-      SELECT (cantidad_producto * valor_unitario) AS total_prod, factura_numero  
-      FROM(
-          SELECT y.cantidad_producto, x.valor_unitario, y.factura_numero  
-          FROM (
-            SELECT id, valor_unitario
-            FROM producto
-          ) AS x
-          INNER JOIN factura_producto AS y ON x.id = y.producto_id
-      ) AS sub_total_prod
-    ) AS total_factura
-    GROUP BY factura_numero
-    ORDER BY subtotal_factura_mayor DESC
-    LIMIT 1
-  ) AS factura_con_compra_mas_cara  
-  INNER JOIN factura AS y_factura ON factura_con_compra_mas_cara.factura_numero = y_factura.numero
-) AS cliente_id_compra_mas_alta
-INNER JOIN cliente AS y_cliente ON cliente_id_compra_mas_alta.cliente_id = y_cliente.id
- ;
+    SELECT SUM(valor_unitario * cantidad_producto *1.19) AS total_factura_mayor, factura_numero  FROM producto
+    JOIN factura_producto ON producto.id=factura_producto.producto_id
+    GROUP BY factura_numero ORDER BY total_factura_mayor DESC LIMIT 1) AS tabla_subtotales
+  JOIN factura ON tabla_subtotales.factura_numero=factura.numero) AS tabla_subtotales_id_cliente
+JOIN cliente ON tabla_subtotales_id_cliente.cliente_id = cliente.id 
+;
 -- ¿Que cliente pagó sobre 1.000.000 de monto? Nota: el ejercicio decía 100, pero dado que puse precios altos subí la cifra, esta vez si se considero IVA
 
-SELECT y_cliente.nombre, clientes_id_compra_mas_un_millon.factura_numero, clientes_id_compra_mas_un_millon.total_factura_mayor_un_millon
+SELECT total_factura_mayor_1000000, factura_numero, cliente_id, nombre
 FROM (
-  SELECT y_factura.cliente_id, factura_con_compra_mas_cara.factura_numero, factura_con_compra_mas_cara.total_factura_mayor_un_millon
+  SELECT total_factura_mayor_1000000, factura_numero, cliente_id 
   FROM (
-    SELECT
-      (sum(total_prod)*1.19) AS total_factura_mayor_un_millon, factura_numero 
-    FROM (
-      SELECT (cantidad_producto * valor_unitario) AS total_prod, factura_numero  
-      FROM(
-          SELECT y.cantidad_producto, x.valor_unitario, y.factura_numero  
-          FROM (
-            SELECT id, valor_unitario
-            FROM producto
-          ) AS x
-          INNER JOIN factura_producto AS y ON x.id = y.producto_id
-      ) AS sub_total_prod
-    ) AS total_factura
-    GROUP BY factura_numero
-    ORDER BY total_factura_mayor_un_millon DESC
-  ) AS factura_con_compra_mas_cara  
-  INNER JOIN factura AS y_factura ON factura_con_compra_mas_cara.factura_numero = y_factura.numero
-) AS clientes_id_compra_mas_un_millon
-INNER JOIN cliente AS y_cliente ON clientes_id_compra_mas_un_millon.cliente_id = y_cliente.id
-WHERE total_factura_mayor_un_millon > 1000000
+    SELECT SUM(valor_unitario * cantidad_producto * 1.19) AS total_factura_mayor_1000000, factura_numero  FROM producto
+    JOIN factura_producto ON producto.id=factura_producto.producto_id
+    GROUP BY factura_numero ORDER BY total_factura_mayor_1000000 DESC) AS tabla_subtotales
+  JOIN factura ON tabla_subtotales.factura_numero=factura.numero) AS tabla_subtotales_id_cliente
+JOIN cliente ON tabla_subtotales_id_cliente.cliente_id = cliente.id
+WHERE total_factura_mayor_1000000 > 1000000 
 ;
 
 -- ¿Cuantos clientes han comprado el producto 6? -- Se cambia por ¿Cuáles cliente compraron el producto 6?
-SELECT clientes_producto6.cliente_id, clientes_producto6.factura_numero, cliente.nombre
-FROM (
-  SELECT y_factura.cliente_id, facturas_item_6.factura_numero
-    FROM (
-    SELECT x.id, y.factura_numero  
-      FROM (
-        SELECT id
-        FROM producto
-      ) AS x
+SELECT clientes_producto6.cliente_id, clientes_producto6.factura_numero, cliente.nombre FROM (
+  SELECT y_factura.cliente_id, facturas_item_6.factura_numero FROM (
+    SELECT x.id, y.factura_numero FROM (
+      SELECT id FROM producto
+    ) AS x
     INNER JOIN factura_producto AS y ON x.id = y.producto_id
     WHERE x.id = 6
-    ) AS facturas_item_6
+  ) AS facturas_item_6
     INNER JOIN factura AS y_factura ON facturas_item_6.factura_numero= y_factura.numero
 ) AS clientes_producto6
 INNER JOIN cliente ON clientes_producto6.cliente_id = cliente.id
